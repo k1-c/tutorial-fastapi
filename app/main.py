c32887dc1c594efefa33df8374bc6ee9507d34b7
shunk-py <auau3700@gmail.com>
2020-05-06 00:38:46 +0900

    feat: query parameters

--- app/main.py
+++ app/main.py
@@ -2,52 +2,49 @@ from enum import Enum
 
 from fastapi import FastAPI
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"message": "hello, world!"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
+# クエリパラメータ
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# クエリパラメータ（boolean）
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: str = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
-# パスは順番に解決される
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+# クエリパラメータ（複数）
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
-# 先に"/me"が定義されてるので、/meにアクセス時には劣後する
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-# 文字列型、列挙型を継承したモデルを作成することで、特定の文字列しかパラメータとして受け付けないようにする
-# また、これは/docsにも反映される
-@app.get("/model/{model_name}")
-async def get_model(model_name: ModelName):
-    # クラスのメンバと比較できる
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    #
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-# ファイル操作の例
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+# クエリパラメータ（Required）
+# 単に初期値を設定しなければRequiedになる
+@app.get("/todos/{todo_id}")
+async def read_user_todo(todo_id: str, needy: str):
+    item = {"item_id": todo_id, "needy": needy}
+    return item
