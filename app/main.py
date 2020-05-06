6431166477d8c1a840ebb79655046d9d8568304e
shunk-py <auau3700@gmail.com>
2020-05-06 14:34:47 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,50 +1,39 @@
-from enum import Enum
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
 
 app = FastAPI()
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.post("/items/")
+async def create_item(item: Item):
+    """
+    Request Bodyを使いたい場合、Pydanticを利用する。
+    Pydanticに定義された型・Bodyが要求される。
+    If its not required, declare default value.
+    """
+    return item
 
 
-# クエリパラメータ
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+# @app.put("/items/{item_id}")
+# async def create_item(item_id: int, item: Item):
+    # Putを利用する場合
+    # return {"item_id": item_id, **item.dict()}
 
 
-# クエリパラメータ（boolean）
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str = None, short: bool = False):
-    item = {"item_id": item_id}
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str = None):
+    """
+    クエリパラメータとPutを利用する
+    """
+    result = {"item_id": item_id, **item.dict()}
     if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-
-# クエリパラメータ（複数）
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-
-# クエリパラメータ（Required）
-# 単に初期値を設定しなければRequiedになる
-@app.get("/todos/{todo_id}")
-async def read_user_todo(todo_id: str, needy: str):
-    item = {"item_id": todo_id, "needy": needy}
-    return item
+        result.update({"q": q})
+    return result
